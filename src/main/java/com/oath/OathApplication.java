47e2cd6a27805bba443cb4c740b283f142482fcd
dovahk11m <choongechobiz@gmail.com>
2025-10-21 09:18:49 +0900

    퍼스트 커밋/ 10.21/ 조충희

---
+++ src/main/java/com/oath/OathApplication.java
@@ -0,0 +1,13 @@
+package com.oath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OathApplication.class, args);
+	}
+
+}
