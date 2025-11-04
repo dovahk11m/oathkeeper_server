4b36bd34a2e9e21e56645a8303ddeff2a7e7ab69
ysb5397 <ysb5397@gmail.com>
2025-11-04 10:42:56 +0900

    장소 ì자연어 처리 방식변경 및 불피필요한 메서드 제거

--- src/main/java/com/oath/recommend_domain/plan/PlanEmbedding.java
+++ src/main/java/com/oath/recommend_domain/plan/PlanEmbedding.java
@@ -66,36 +66,10 @@ public class PlanEmbedding {
                 .map((participant) -> participant.getMember().getUsername())
                 .toList();
 
-        // 참여 인원 자연어 처리
-        for (String s : memberNames) {
-            naturalLanguage = naturalLanguage.concat(s);
-
-            if (!s.equals(memberNames.getLast()))
-                naturalLanguage = naturalLanguage.concat(", ");
-        }
-
-        String lastMemberName = memberNames.getLast();
-
-        if (hasFinalConsonant(lastMemberName.charAt(lastMemberName.length() - 1))) {
-            naturalLanguage = naturalLanguage.concat("과 만났다.");
-        } else {
-            naturalLanguage = naturalLanguage.concat("와 만났다.");
-        }
+        naturalLanguage = naturalLanguage.concat(plan.getParticipants().size() + "명과 만났다.");
 
         System.out.println(naturalLanguage);
         return naturalLanguage;
-    }
-
-    private static boolean hasFinalConsonant(char ch) {
-        if (ch < 0xAC00 || ch > 0xD7A3) {
-            // 한글 음절이 아니면 false
-            return false;
-        }
-
-        int baseCode = ch - 0xAC00;
-        int finalConsonantIndex = baseCode % 28;
-
-        return finalConsonantIndex != 0;
     }
 
     // 시간 포맷팅
