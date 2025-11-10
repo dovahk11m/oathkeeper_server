88b45ac843c8da24249ff95c6a528440ec777482
dovahk11m <choongechobiz@gmail.com>
2025-11-10 14:40:01 +0900

    페이저블 의존성 관련 수정/1110 조충희

--- src/main/java/com/oath/domain/place_tag_plan/place/PlaceRepository.java
+++ src/main/java/com/oath/domain/place_tag_plan/place/PlaceRepository.java
@@ -1,10 +1,10 @@
 package com.oath.domain.place_tag_plan.place;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
